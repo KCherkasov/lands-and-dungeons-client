58307ca982f95cdf81872f3868c8e2986d131022
zonder129 <havofavo@mail.ru>
2018-05-03 01:48:33 +0300

    preferences add

---
+++ core/src/ru/kvvartet/lndclient/client/controller/Preferences.java
@@ -0,0 +1,12 @@
+package ru.kvvartet.lndclient.client.controller;
+
+public final class Preferences {
+    public static final String SERVER_HOST = "localhost";
+    public static final Integer SERVER_PORT = 8081;
+    public static final String SIGNUP_URI = "/signup";
+    public static final String SIGNIN_URI = "/signin";
+    public static final String SESSION_URI = "/session";
+    public static final String SETTINGS_URI = "/settings";
+    public static final String SCOREBOARD_URI = "/scoreboard";
+    public static final String SIGNPUT_URI = "/signout";
+}
