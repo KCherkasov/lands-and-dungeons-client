99250b6704e0c48cc8763a5d1ed096e5e736d443
zonder129 <havofavo@mail.ru>
2018-05-03 01:48:07 +0300

    client controller add

--- core/src/ru/kvvartet/lndclient/client/controller/ClientController.java
+++ core/src/ru/kvvartet/lndclient/client/controller/ClientController.java
@@ -1,7 +1,21 @@
 package ru.kvvartet.lndclient.client.controller;
 
+import ru.kvvartet.lndclient.client.controller.model.User;
+
 public interface ClientController {
     void init();
 
     void update(float timeDelta);
+
+    void signUp(User user);
+
+    void signIn(User user);
+
+    User getSession();
+
+    void changeProfile(User newUser);
+
+    void signOut();
+
+
 }
