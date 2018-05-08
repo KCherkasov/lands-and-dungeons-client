6d67b4ccbef80e876d25098d820b5a043b534b9f
zonder129 <havofavo@mail.ru>
2018-05-08 12:07:50 +0300

    signin state update with backend support

--- core/src/ru/kvvartet/lndclient/client/states/state/LoginState.java
+++ core/src/ru/kvvartet/lndclient/client/states/state/LoginState.java
@@ -1,10 +1,23 @@
 package ru.kvvartet.lndclient.client.states.state;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Net;
+import com.badlogic.gdx.net.HttpRequestBuilder;
+import com.badlogic.gdx.net.HttpStatus;
 import com.badlogic.gdx.scenes.scene2d.Actor;
-import com.badlogic.gdx.scenes.scene2d.ui.*;
+import com.badlogic.gdx.scenes.scene2d.ui.CheckBox;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.TextField;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+
 import org.jetbrains.annotations.NotNull;
+
 import ru.kvvartet.lndclient.assets.configs.SgxUiSkinKeys;
+import ru.kvvartet.lndclient.client.network.NetworkConfig;
+import ru.kvvartet.lndclient.client.network.model.User;
 import ru.kvvartet.lndclient.client.states.manager.StateManager;
 
 public class LoginState extends AbstractAuthorizationState {
@@ -90,14 +103,57 @@ public class LoginState extends AbstractAuthorizationState {
         // TODO: if login attempt was successful - go to ingame state, otherwise - tell what's wrong
     }
 
-    private void submitOnClickCallback() {
-        // TODO: login logic here
+    @Override
+    protected String validateInput() {
+        /*
+         * login validation
+         * */
 
-        // working with login saving (NOTE: we don't save passwords anyways)
-        getSettings().setLoginSavingPolicy(loginSavingPolicy.isChecked());
-        getSettings().save();
-        getSettings().setLastUsedLogin(getSettings().isLoginSavingEnabled()
-                ? login.getText() : EMPTY_FILLER);
+        if(login.getText().length() < 6) {
+            return "Login is too short";
+        }
+
+        /*
+         * password validation
+         * */
+
+        if(password.getText().length() < 6) {
+            return "Password is too short";
+        }
+
+        return null;
+    }
+
+    private void submitOnClickCallback() {
+        if (validateInput() != null) {
+            //TODO: Widget with error
+            System.out.println("INPUT NOT VALID BECAUSE: " + validateInput());
+        } else {
+            final User user = new User(login.getText(), null, password.getText());
+            signInRequest(user);
+
+            // working with login saving (NOTE: we don't save passwords anyways)
+            getSettings().setLoginSavingPolicy(loginSavingPolicy.isChecked());
+            getSettings().save();
+            getSettings().setLastUsedLogin(getSettings().isLoginSavingEnabled()
+                    ? login.getText() : EMPTY_FILLER);
+        }
+    }
+
+    private void signInRequest(User user) {
+        final HttpRequestBuilder httpRequestBuilder = new HttpRequestBuilder();
+        final Net.HttpRequest httpRequest = httpRequestBuilder.newRequest()
+                .method(Net.HttpMethods.POST)
+                //.header("Cookie", prefs.getString("JSESSIONID", null))
+                .header("Content-Type", "application/json")
+                .url(NetworkConfig.READY_URL + NetworkConfig.SIGNIN_URI)
+                .content(json.toJson(user))
+                .includeCredentials(true)
+                .build();
+        System.out.println(json.toJson(user, User.class));
+        System.out.println("URL TO SEND: " + NetworkConfig.READY_URL + NetworkConfig.SIGNIN_URI);
+        System.out.println("SIGN IN SENDED");
+        Gdx.net.sendHttpRequest(httpRequest, this);
     }
 
     private void signUpOnClickCallback() {
@@ -108,5 +164,27 @@ public class LoginState extends AbstractAuthorizationState {
     private void backOnClickCallback() {
         stateManager.requestStatePop();
         stateManager.requestStatePush(new MainMenuState(stateManager));
+    }
+
+    @Override
+    public void handleHttpResponse(Net.HttpResponse httpResponse) {
+        if(httpResponse.getStatus().getStatusCode() >= HttpStatus.SC_OK
+                && httpResponse.getStatus().getStatusCode() < HttpStatus.SC_MULTIPLE_CHOICES) {
+            //TODO: Widget with successful registration and after "ok" move to login screen
+            System.out.println(httpResponse.getResultAsString());
+        } else {
+            //TODO: Widget with error response from server
+            System.out.println(httpResponse.getResultAsString());
+        }
+    }
+
+    @Override
+    public void failed(Throwable t) {
+
+    }
+
+    @Override
+    public void cancelled() {
+
     }
 }
