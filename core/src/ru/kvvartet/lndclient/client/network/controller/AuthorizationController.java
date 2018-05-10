3cb8245909e8b74f55a53686532ae230b78bf8f6
zonder129 <havofavo@mail.ru>
2018-05-10 16:06:50 +0300

    authorization controller add

---
+++ core/src/ru/kvvartet/lndclient/client/network/controller/AuthorizationController.java
@@ -0,0 +1,17 @@
+package ru.kvvartet.lndclient.client.network.controller;
+
+import com.badlogic.gdx.Net;
+
+import ru.kvvartet.lndclient.client.network.model.User;
+
+public interface AuthorizationController {
+    void signUp(User user, Net.HttpResponseListener httpResponseListener);
+
+    void signIn(User user, Net.HttpResponseListener httpResponseListener);
+
+    void userInformation(Net.HttpResponseListener httpResponseListener);
+
+    void changeProfile(User user, Net.HttpResponseListener httpResponseListener);
+
+    void signOut(User user, Net.HttpResponseListener httpResponseListener);
+}
---
+++ core/src/ru/kvvartet/lndclient/client/network/controller/AuthorizationControllerImpl.java
@@ -0,0 +1,71 @@
+package ru.kvvartet.lndclient.client.network.controller;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Net;
+import com.badlogic.gdx.net.HttpRequestBuilder;
+import com.badlogic.gdx.net.HttpRequestHeader;
+import com.badlogic.gdx.utils.Json;
+import com.badlogic.gdx.utils.JsonWriter;
+
+import ru.kvvartet.lndclient.client.network.NetworkConfig;
+import ru.kvvartet.lndclient.client.network.model.User;
+import ru.kvvartet.lndclient.preferences.AuthorizationPreferences;
+
+public class AuthorizationControllerImpl implements  AuthorizationController {
+
+    protected final Json json = new Json();
+    protected AuthorizationPreferences preferences = AuthorizationPreferences.getInstance();
+
+    public AuthorizationControllerImpl() {
+        json.setOutputType(JsonWriter.OutputType.json);
+    }
+
+    @Override
+    public void signUp(User user, Net.HttpResponseListener httpResponseListener) {
+        final HttpRequestBuilder httpRequestBuilder = new HttpRequestBuilder();
+        final Net.HttpRequest httpRequest = httpRequestBuilder.newRequest()
+                .method(Net.HttpMethods.POST)
+                .header(HttpRequestHeader.ContentType, "application/json")
+                .url(NetworkConfig.READY_URL + NetworkConfig.SIGNUP_URI)
+                .content(json.toJson(user))
+                .includeCredentials(true)
+                .build();
+        System.out.println(json.toJson(user, User.class));
+        System.out.println("URL TO SEND: " + NetworkConfig.READY_URL + NetworkConfig.SIGNUP_URI);
+        System.out.println("SIGN UP SENDED");
+        Gdx.net.sendHttpRequest(httpRequest, httpResponseListener);
+    }
+
+    @Override
+    public void signIn(User user, Net.HttpResponseListener httpResponseListener) {
+        final HttpRequestBuilder httpRequestBuilder = new HttpRequestBuilder();
+        System.out.println("Cookie request: " + AuthorizationPreferences.AUTHORIZATION_COOKIE_ID + '=' +preferences.getSessionId());
+        final Net.HttpRequest httpRequest = httpRequestBuilder.newRequest()
+                .method(Net.HttpMethods.POST)
+                .header(HttpRequestHeader.Cookie, AuthorizationPreferences.AUTHORIZATION_COOKIE_ID + '=' +preferences.getSessionId())
+                .header(HttpRequestHeader.ContentType, "application/json")
+                .url(NetworkConfig.READY_URL + NetworkConfig.SIGNIN_URI)
+                .content(json.toJson(user))
+                .includeCredentials(true)
+                .build();
+        System.out.println(json.toJson(user, User.class));
+        System.out.println("URL TO SEND: " + NetworkConfig.READY_URL + NetworkConfig.SIGNIN_URI);
+        System.out.println("SIGN IN SENDED");
+        Gdx.net.sendHttpRequest(httpRequest, httpResponseListener);
+    }
+
+    @Override
+    public void userInformation(Net.HttpResponseListener httpResponseListener) {
+
+    }
+
+    @Override
+    public void changeProfile(User user, Net.HttpResponseListener httpResponseListener) {
+
+    }
+
+    @Override
+    public void signOut(User user, Net.HttpResponseListener httpResponseListener) {
+
+    }
+}
